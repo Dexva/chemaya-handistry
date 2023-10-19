b096ef8001b2df619e50fe5133fcb351c6bf6fe5
Davis Magpantay <magpantay_d@yahoo.com>
2023-10-19 13:23:05 +0800

    [UPD] Serving custom-trained gesture recognizer task file - Just use the write modelAssetPath in /home.html (mediapipe deefault or our jueves model)

--- hgnui-test/index.js
+++ hgnui-test/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('path')
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: { origin: "http://localhost:3000" }
 });
 
+// let __dirname = "/Users/davisnicholomagpantay/Documents/Grade 12 Files/Research 3/Revamped Handistry/handistry/hgnui-test";
+
+app.use(express.static(path.join(__dirname,"public")));
 app.get('/', (req, res) => {
-    res.sendFile('/Users/davisnicholomagpantay/Documents/Grade 12 Files/Research 3/Revamped Handistry/handistry/hgnui-test/home.html');
+    res.sendFile(__dirname + "/home.html");
 });
 
 io.on('connection', (socket) => {
